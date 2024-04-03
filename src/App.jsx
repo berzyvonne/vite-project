5996e7cc576e0b196a45c1e942e90c804e6106e8
berzyvonne <berzyvonne@gmail.com>
2024-04-02 22:39:30 -0400

    puppy data with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
   const [count, setCount] = useState(0)
-
+console.log(puppies)
+const [puppies, setPuppies] = useState(puppyList)
   return (
     <>
       <div>
-        {/* <a href="https://vitejs.dev" target="_blank">
+        {
+        puppies.map((puppy) => {
+          return <p>{puppy.name}</p>
+        })
+        /* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">

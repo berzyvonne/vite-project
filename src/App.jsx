7b9be22b48ab581145a0414dce17d812cce040a9
berzyvonne <berzyvonne@gmail.com>
2024-04-07 19:54:33 -0400

    Select a Featured Puppy

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-import {puppyList} from './data.js'
+import { puppyList } from "./data.js";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
-
+  const [count, setCount] = useState(0);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log(puppies);
+  
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
   return (
     <>
       <div>
+        <div> {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}</div>
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>; // Added key prop for each item in the map
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          ); // Added key prop for each item in the map
         })}
       </div>
       <h1>Vite + React</h1>
@@ -21,6 +42,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
